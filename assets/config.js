e823eef255290c780f9d014fe217de11fcebc46b
Brendon <brendonbarringer@gmail.com>
2018-07-03 13:15:46 -0500

    seperated api keys and added file to .gitignore

---
+++ assets/config.js
@@ -0,0 +1,4 @@
+const TMS_API_KEY1 = "qcw98up43djnn38bmwsh739a";
+const TMS_API_KEY2 = "rgf2xy8vkcqdpwq3ksjz44b5";
+const omdbapi = 'ccaa8d15'
+const ytkey = 'AIzaSyA48DgSrZgc7HxXqMqf1nwRIgn7pfYq_Ig'
