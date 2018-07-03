2da42a7a76476107a533fc9f2aa58ef987507e2f
Brendon <brendonbarringer@gmail.com>
2018-07-03 14:06:19 -0500

    fixed omdb error

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 const TMS_API_KEY1 = "qcw98up43djnn38bmwsh739a";
 const TMS_API_KEY2 = "rgf2xy8vkcqdpwq3ksjz44b5";
-const omdbapi = 'ccaa8d15'
+const omdbkey = 'ccaa8d15'
 const ytkey = 'AIzaSyA48DgSrZgc7HxXqMqf1nwRIgn7pfYq_Ig'
